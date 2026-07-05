0665fba6c1fc129ec32cd86c4cf1e5870c64fa70
zhl <3340488782@qq.com>
2026-07-05 14:40:51 +0800

    准备调视觉伺服

--- competition/scripts/复位.py
+++ competition/scripts/复位.py
@@ -4,5 +4,11 @@ arm = AkaiFr()
 arm.set_speed(50)
 tcf = [0, 0, 0, 0, 0, 0.0]
 arm.set_tcf(1, tcf)
-shooting_angle = [-475.0334849461607,-156.55351754676806,180.83411546358315 , -180, 0, 90]
-arm.set_tool_pose(shooting_angle)
+# ret, tool_pose = arm.get_tool_pose()
+# print(f"工具坐标系姿态 [XYZRPY]: {tool_pose}")
+shooting_angle = [-9,-351.252,320.273, 180, 0, -170]
+x=shooting_angle[1]
+for i in range(30):
+    shooting_angle = [-9,x,320.273, 180, 0, -170]
+    arm.set_tool_pose(shooting_angle)
+    x=x-1
